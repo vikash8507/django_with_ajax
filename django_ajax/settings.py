3716205e999674549968ff31c1ee0d906262bb26
vikash1998think <vikash.kumar@thinkitive.com>
2022-12-17 19:47:07 +0530

    added custom context processor

--- django_ajax/settings.py
+++ django_ajax/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'form_ajax.context_processors.access_banner_image'
             ],
         },
     },
