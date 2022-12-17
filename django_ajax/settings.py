7c69cb1f4dfa86c0133cdbd08005a7e47658fc92
vikash1998think <vikash.kumar@thinkitive.com>
2022-12-17 19:51:47 +0530

    added custom middleware

--- django_ajax/settings.py
+++ django_ajax/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'form_ajax.middlewares.CustomMiddleware'
 ]
 
 ROOT_URLCONF = 'django_ajax.urls'
